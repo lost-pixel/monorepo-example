bc4c59d1149fcf6a4c4060bdc66c0fa0518ed2ee
d-ivashchuk <dimaiv@me.com>
2023-04-16 23:47:35 +0200

    add lost pixel integration

---
+++ apps/web/lostpixel.config.ts
@@ -0,0 +1,11 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+//@ts-expect-error - types are broken, need fix
+export const config: CustomProjectConfig = {
+  pageShots: {
+    pages: [{ path: "/", name: "web" }],
+    baseUrl: "http://172.17.0.1:3000",
+  },
+  lostPixelProjectId: "clgjsy7vk01vajs0er6790hjs",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
