4861ca7744435d8de0a3ea16412bc5baa0489df8
d-ivashchuk <dimaiv@me.com>
2023-04-17 12:06:59 +0200

    remove additional tested routes

--- apps/docs/lostpixel.config.ts
+++ apps/docs/lostpixel.config.ts
@@ -3,10 +3,7 @@ import { CustomProjectConfig } from "lost-pixel";
 //@ts-expect-error - types are broken, need fix
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [
-      { path: "/", name: "docs-home" },
-      { path: "/main", name: "main" },
-    ],
+    pages: [{ path: "/", name: "docs-home" }],
     baseUrl: "http://172.17.0.1:3001",
   },
   lostPixelProjectId: "clgjxquki02dakn0ec4r6lsz5",
--- apps/web/lostpixel.config.ts
+++ apps/web/lostpixel.config.ts
@@ -3,10 +3,7 @@ import { CustomProjectConfig } from "lost-pixel";
 //@ts-expect-error - types are broken, need fix
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [
-      { path: "/", name: "docs-home" },
-      { path: "/main", name: "main" },
-    ],
+    pages: [{ path: "/", name: "docs-home" }],
     baseUrl: "http://172.17.0.1:3000",
   },
   lostPixelProjectId: "clgjsy7vk01vajs0er6790hjs",
